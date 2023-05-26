ec28b23f6e003d8d17a84f97fae3d149a43b7890
javasupercoder <130356943+javasupercoder@users.noreply.github.com>
2023-05-26 07:24:07 +0530

    Update Multi.java

--- src/com/demo/Multi.java
+++ src/com/demo/Multi.java
@@ -9,7 +9,7 @@ public class Multi {
 		a=30;
 		a=40;
 		System.out.println("Multiplication : "+(a*b));
-		//Added from github
+		//Added from github by sujit
 		a=50;
 		a=60;
 		System.out.println("Multiplication : "+(a*b));
